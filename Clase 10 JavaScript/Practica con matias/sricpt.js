d19ed23cc6bab8e8b6d220ab56809e37d039781f
Alan Cadena <alancadena12@gmail.com>
2022-10-01 15:51:25 -0300

    clase10

---
+++ Clase 10 JavaScript/Practica con matias/sricpt.js
@@ -0,0 +1,40 @@
+//alert("Hola Mundo")
+//Tipos De Datos
+
+//TIPOS DE DATOS PRIMITIVOS(3)
+
+//.1 Boolean: true: 1 / false: 0
+
+// .2 Number: entero, ejemplos (1,2,3,4,5,-1,-2,-3,-4,-5,etc)
+
+// .3 Strings (cadenas de texto): "hola" ,
+
+//OTROS TIPOS DE DATOS!
+
+//NAN = Not A Number
+
+//Undefined: Indefinido
+//Null:      Nulo
+
+
+//var  /  let  /  const //
+
+//Operadores:
+
+// + Es el operador de Suma(Para strings es concatenar)(Para numbers es sumar)
+// = Es el Operador Asignacion
+
+// ES6+ = Es el JS moderno (actual) No se usa var
+//var megustajugar = true
+
+//Variables
+//let numerofavorito = 455
+
+//Constantes
+//const pi= 3.14
+
+//Practico de Yanel
+let nombre = prompt ("Ingrese Nombre")
+let apellido = prompt ("Ingrese Su Apellido")
+let salida = (nombre + ' ' + apellido)
+alert (salida)
